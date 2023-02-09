f224084f5a62c73fb9276ba8c13514ec6434d42e
Alvin Li <aflip24@gmail.com>
2023-02-09 07:17:05 -0600

    finished looping lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let card = [];
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      card.push(message);
+    }
+    return card;
+  }
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
